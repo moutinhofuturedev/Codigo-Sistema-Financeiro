fc9184af28b61857ddd6e3cd5860daa3efbd4795
Paulo Moutinho <paulo_vicali@icloud.com>
2021-12-29 22:21:42 -0300

    Primeiro arquivo js add no projeto

---
+++ script.js
@@ -0,0 +1,12 @@
+Modal = {
+    open() {
+        document.querySelector('.modal-overlay').classList.add('active')
+    },
+    close() {
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
+
+
+
+
