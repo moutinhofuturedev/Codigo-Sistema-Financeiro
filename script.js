3fa27f3ddf2e85d79e3e6ae4ea89a5708f48d8b0
Paulo Moutinho <paulo_vicali@icloud.com>
2022-01-01 22:18:31 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -11,42 +11,56 @@ const Modal = {
     }
 }
 
-const transactions = [{
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '26/12/2021',
+// Armazenameto de informações 
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem('dev.finances:transactions')) || []
     },
-    {
-        id: 2,
-        description: 'Criação de Web Site',
-        amount: 500000,
-        date: '26/12/2021',
-    },
-    {
-        id: 3,
-        description: 'Internet',
-        amount: -20000,
-        date: '26/12/2021',
-    },
-    {
-        id: 4,
-        description: 'App',
-        amount: 400000,
-        date: '26/12/2021',
-    }
 
-]
+    set(transactions) {
+        localStorage.setItem('dev.finances:transactions', JSON.stringify(transactions))
+    }
+        
+}
 
 const Transaction = {
+    all: Storage.get(),
+
+    add(transaction) {
+        Transaction.all.push(transaction)
+
+        App.reload()    
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+
+    },
+
     incomes() {
-        // somar as entradas
+        let income = 0
+        Transaction.all.forEach((transaction) => {
+            if (transaction.amount > 0) {
+                income += transaction.amount
+            }
+        })
+        return income
     },
+
     expenses() {
-        // somar as saídas
+        let expense = 0
+        Transaction.all.forEach((transaction) => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount
+            }
+        })
+        return expense
     },
+
     total() {
-        // entradas e saídas
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
@@ -56,12 +70,13 @@ const DOM = {
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
 
         DOM.transactionsContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
 
         const amount = Utils.formatCurrency(transaction.amount)
@@ -73,15 +88,43 @@ const DOM = {
                         <td class="${CSSclass}">${amount}</td>
                         <td class="date">${transaction.date}</td>
                         <td>
-                            <img src="assets/minus.svg" alt="Remover Transação">
+                            <img onclick='Transaction.remove(${index})' onclick='cursorPointer' src="assets/minus.svg" alt="Remover Transação">
                         </td>
                     
                     `
-                    return html
+        return html
+    },
+
+
+
+    // atualização (formatação) das entradas,saídas e total
+    upDateBalance() {
+        document.getElementById('incomesDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes())
+
+        document.getElementById('expensesDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses())
+
+        document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ''
     }
 }
 
+// formatação completa de um número normal em moeda com caracteres nativo 
 const Utils = {
+    formatAmount(value) {
+        value = Number(value) * 100
+
+        return value
+    },
+
+    formatDate(date) {
+        const splittedDate = date.split('-')
+
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+
     formatCurrency(value) {
         const signal = Number(value) < 0 ? '-' : ''
 
@@ -99,7 +142,87 @@ const Utils = {
     }
 }
 
-// Esta função foi criada para contar o objeto de arrays da const transactions 
-transactions.forEach(function(transaction) {
-    DOM.addTransaction(transaction)
-})
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+
+    validateFields() {
+        const {description, amount, date} = Form.getValues()
+
+        if(description.trim() === '' || amount.trim() === '' || date.trim() === '') {
+            throw new Error('Por favor,preencha todos os campos')
+        }
+    },
+
+    formatValues() {
+        let {description, amount, date} = Form.getValues()
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+
+    },
+
+    clearFields() {
+       Form.description.value = ''
+       Form.amount.value = ''
+       Form.date.value = ''
+    },
+
+    submit(event) {
+        event.preventDefault()
+
+        try {
+            Form.validateFields()
+            const transaction = Form.formatValues()
+            // salvar os dados 
+            Transaction.add(transaction)
+            // apagar os dados do formulário 
+            Form.clearFields()
+
+            Modal.close()
+
+        } catch (error) {
+            alert(error.message)
+        }
+
+        // Form.formatData()
+    }
+}
+
+const App = {
+    init() {
+        // Esta função foi criada para contar o objeto de arrays da const transactions 
+        Transaction.all.forEach(DOM.addTransaction)
+
+        // Depois de registrar o upDateBalance dentro do DOM,agora chamo a função upDateBalance
+        DOM.upDateBalance()
+        
+        Storage.set(Transaction.all)
+    },
+
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    }
+}
+
+App.init()
+
+
+
+
